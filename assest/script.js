e4635f002031b6b0d567f3914757af4ec71b8267
Chris.Franklin <cfrank306@gmail.com>
2021-10-05 09:49:51 -0500

    fixed timer

--- assest/script.js
+++ assest/script.js
@@ -2,6 +2,7 @@ var startButton = document.getElementById("start-btn")
 var answerBtnEl = document.getElementById("answer-btn")
 var questionsEl = document.getElementById("questions")
 var questionContainerEl = document.getElementById("question-container")
+var h2El = document.getElementById("h2")
 
 
 let shuffledQuestions, currentQuestionsIndex
@@ -22,21 +23,25 @@ function setTime() {
     }, 1000);
 }
 
-
+// starts the game
 function startGame() {
     console.log("Started...Yay!!!");
     startButton.classList.add("hide");
     answerBtnEl.classList.remove("hide");
+    h2El.classList.add("hide");
     shuffledQuestions = questions.sort(() => Math.random() - .5)
     currentQuestionsIndex = 0
+    setTime()
     showNextQuestion()
 }
 
+// shows the next question
 function showNextQuestion() {
     resetPage()
     showNewQuestion(shuffledQuestions[currentQuestionsIndex])
 }
 
+// shows a "new" question in the "next" question function
 function showNewQuestion(question) {
     questionsEl.innerText = question.question
     question.answers.forEach(answer => {
@@ -51,6 +56,7 @@ function showNewQuestion(question) {
     });
 }
 
+// resets the page, to allow for filling answer buttons and question with new text
 function resetPage() {
     clearStatusClass(document.body)
     while (answerBtnEl.firstChild) {
@@ -58,6 +64,7 @@ function resetPage() {
     }
 }
 
+// selects answers from the question array below
 function pickAnswer(e) {
     var pickedButton = e.target
     var correct = pickedButton.dataset.answers
@@ -90,46 +97,46 @@ function clearStatusClass(element) {
 var questions = [{
     question: "Commonly used data types do NOT include:",
     answers: [
-        {text: "strings", correct: false},
-        {text: "booleans", correct: false},
-        {text: "alerts", correct: true},
-        {text: "square brackets", correct: false}
+        { text: "strings", correct: false },
+        { text: "booleans", correct: false },
+        { text: "alerts", correct: true },
+        { text: "square brackets", correct: false }
     ]
 },
 {
     question: "The condition in an if/else statement is enclosed within ____.",
     answers: [
-        {text: "quotes", correct: false},
-        {text: "curly brackets", correct: false},
-        {text: "parentheses", correct: true},
-        {text: "square brackets", correct: false}
+        { text: "quotes", correct: false },
+        { text: "curly brackets", correct: false },
+        { text: "parentheses", correct: true },
+        { text: "square brackets", correct: false }
     ]
 },
 {
     question: "Arrays in JavaScript can be used to store _____.",
     answers: [
-        {text: "numbers and strings", correct: false},
-        {text: "other arrays", correct: false},
-        {text: "booleans", correct: false},
-        {text: "all of the above", correct: true}
+        { text: "numbers and strings", correct: false },
+        { text: "other arrays", correct: false },
+        { text: "booleans", correct: false },
+        { text: "all of the above", correct: true }
     ]
 },
 {
     question: "String values must be enclosed within _____ when being assigned to variables.",
     answers: [
-        {text: "commas", correct: false},
-        {text: "curly brackets", correct: false},
-        {text: "quotes", correct: true},
-        {text: "parentheses", correct: false}
+        { text: "commas", correct: false },
+        { text: "curly brackets", correct: false },
+        { text: "quotes", correct: true },
+        { text: "parentheses", correct: false }
     ]
 },
 {
     question: "A very useful tool used during development and debugging for printing content to the debugger is:",
     answers: [
-        {text: "JavaScript", correct: false},
-        {text: "bash", correct: false},
-        {text: "for loop", correct: false},
-        {text: "console.log", correct: true}
+        { text: "JavaScript", correct: false },
+        { text: "bash", correct: false },
+        { text: "for loop", correct: false },
+        { text: "console.log", correct: true }
     ]
 },
 ]
